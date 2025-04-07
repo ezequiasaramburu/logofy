fe068081f32d714016803dd89ae738c6e75b95f5
Ezequias <ezequias.aramburu@gmail.com>
2025-04-07 17:11:34 +0200

    update default values

--- src/constants/defaults.ts
+++ src/constants/defaults.ts
@@ -1,15 +1,15 @@
 // Background defaults
-export const DEFAULT_BACKGROUND_COLOR = "linear-gradient(45deg, rgb(33, 27, 212) 23%, rgb(11, 197, 225) 94%)";
+export const DEFAULT_BACKGROUND_COLOR = "linear-gradient(45deg, rgb(33, 27, 212) 23%, rgb(15, 205, 234) 94%)";
 export const DEFAULT_BACKGROUND_ROUNDED = 80;
 export const DEFAULT_BACKGROUND_PADDING = 45;
 
 // Icon defaults
 export const DEFAULT_ICON_SIZE = 350;
 export const DEFAULT_ICON_ROTATE = 0;
-export const DEFAULT_ICON_BORDER_WIDTH = 2.2;
+export const DEFAULT_ICON_BORDER_WIDTH = 1.6;
 export const DEFAULT_ICON_BORDER_COLOR = "#fff";
-export const DEFAULT_ICON_FILL_COLOR = "rgba(255, 255, 255, 0)";
-export const DEFAULT_ICON = "Activity";
+export const DEFAULT_ICON_FILL_COLOR = "#FF8C00";
+export const DEFAULT_ICON = "Rocket";
 
 // Text defaults
 export const DEFAULT_TEXT_SIZE = 40;

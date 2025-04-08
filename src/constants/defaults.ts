b25b15214f6d6532d1709029ad9e0f64db55e172
Ezequias <ezequias.aramburu@gmail.com>
2025-04-08 15:16:08 +0200

    add default text

--- src/constants/defaults.ts
+++ src/constants/defaults.ts
@@ -1,7 +1,7 @@
 // Background defaults
 export const DEFAULT_BACKGROUND_COLOR = "linear-gradient(45deg, rgb(33, 27, 212) 23%, rgb(15, 205, 234) 94%)";
 export const DEFAULT_BACKGROUND_ROUNDED = 80;
-export const DEFAULT_BACKGROUND_PADDING = 45;
+export const DEFAULT_BACKGROUND_PADDING = 30;
 
 // Icon defaults
 export const DEFAULT_ICON_SIZE_DESKTOP = 350;
@@ -13,9 +13,9 @@ export const DEFAULT_ICON_FILL_COLOR = "#FF8C00";
 export const DEFAULT_ICON = "Rocket";
 
 // Text defaults
-export const DEFAULT_TEXT_SIZE = 40;
+export const DEFAULT_TEXT_SIZE = 60;
 export const DEFAULT_TEXT_COLOR = "#fff";
-export const DEFAULT_TEXT = "";
+export const DEFAULT_TEXT = "SimpleLogo";
 export const DEFAULT_TEXT_POSITION_X = 50;
-export const DEFAULT_TEXT_POSITION_Y = 80;
+export const DEFAULT_TEXT_POSITION_Y = 88;
 export const DEFAULT_HIDE_ICON = false; 
